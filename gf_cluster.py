9606bebded37d6323e58d23094137b3779d46afa
Ilari Liukko <ilari.liukko@iki.fi>
2015-11-12 14:28:42 +0200

    Added possibility to use password-file for authenticating to secured domain

--- gf_cluster.py
+++ gf_cluster.py
@@ -8,7 +8,7 @@ def main():
             name=dict(required=True),
             admin_user=dict(required=False, default='admin'),
             password_file=dict(required=False),
-            glassfish_path=dict(required=True),
+            glassfish_path=dict(required=True, default='/opt/glassfish3'),
             state=dict(choices=['absent', 'present'], default='present')
         ),
         supports_check_mode=True
@@ -18,8 +18,6 @@ def main():
 
     cluster_name = module.params['name']
     state = module.params['state']
-
-    password_file = module.params['password_file']
 
     asadmin_command = module.get_bin_path('asadmin', True, [glassfish_path + '/bin/'])
 
@@ -39,14 +37,29 @@ def main():
 
 def execute_delete_cluster(asadmin_command, cluster_name, module):
     if module.check_mode is False:
-        module.run_command([asadmin_command, '--user', (module.params['admin_user']), 'delete-cluster', cluster_name], check_rc=True)
+        execution_args = create_execution_args(module, asadmin_command, 'delete-cluster', cluster_name)
+
+        module.run_command(execution_args, check_rc=True)
 
 
 def execute_create_cluster(module, asadmin_command, cluster_name):
     if module.check_mode is False:
-        module.run_command(
-            [asadmin_command, '--user', (module.params['admin_user']), 'create-cluster', cluster_name],
-            check_rc=True)
+        execution_args = create_execution_args(module, asadmin_command, 'create-cluster', cluster_name)
+
+        module.run_command(execution_args, check_rc=True)
+
+
+def create_execution_args(module, asadmin_command, command, cluster_name):
+    execution_args = [asadmin_command]
+    # add admin user to command
+    execution_args.extend(['--user', module.params['admin_user']])
+    # add password file (if one was provided)
+    if module.params['password_file'] is not None:
+        execution_args.extend(['--passwordfile', module.params['password_file']])
+
+    # add actual command and parameters
+    execution_args.extend([command, cluster_name])
+    return execution_args
 
 
 def is_existing_cluster(module, asadmin_command, domain_name):
