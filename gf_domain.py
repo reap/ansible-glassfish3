e8d58f1218b32f9dbb21a7b3023b2ca3269dbb94
Ilari Liukko <ilari.liukko@iki.fi>
2015-11-12 13:38:09 +0200

    Added possibility also to start (state=running) and stop (state=stopped) domain.
    Module checks domains state and won't try to start/stop it if not required.

--- gf_domain.py
+++ gf_domain.py
@@ -6,11 +6,10 @@ def main():
     module = AnsibleModule(
         argument_spec=dict(
             name=dict(required=True),
-            domain=dict(required=True),
             admin_user=dict(required=False, default='admin'),
             password_file=dict(required=False),
             glassfish_path=dict(required=True),
-            state=dict(choices=['absent', 'present'], default='present')
+            state=dict(choices=['absent', 'present', 'running', 'stopped'], default='present')
         ),
         supports_check_mode=True
     )
@@ -26,14 +25,33 @@ def main():
 
     enviroment_changed = False
 
-    domain_exists = is_existing_domain(module, asadmin_command, domain_name)
+    domain_exists, domain_running = is_existing_domain(module, asadmin_command, domain_name)
 
-    if not domain_exists and state == 'present':
-        execute_create_domain(module, asadmin_command, domain_name)
-        enviroment_changed = True
-    elif domain_exists and state == 'absent':
-        execute_delete_domain(asadmin_command, domain_name, module)
-        enviroment_changed = True
+    if state == 'present':
+        if not domain_exists:
+            execute_create_domain(module, asadmin_command, domain_name)
+            enviroment_changed = True
+    elif state == 'absent':
+        if domain_exists:
+            if domain_running:
+                execute_stop_domain(asadmin_command, domain_name, module)
+
+            execute_delete_domain(asadmin_command, domain_name, module)
+            enviroment_changed = True
+    elif state == 'running':
+        if not domain_exists:
+            execute_create_domain(module, asadmin_command, domain_name)
+            enviroment_changed = True
+        if not domain_running:
+            execute_start_domain(asadmin_command, domain_name, module)
+            enviroment_changed = True
+    elif state == 'stopped':
+        if not domain_exists:
+            execute_create_domain(module, asadmin_command, domain_name)
+            enviroment_changed = True
+        if not domain_running:
+            execute_stop_domain(asadmin_command, domain_name, module)
+            enviroment_changed = True
 
     module.exit_json(changed=enviroment_changed)
 
@@ -50,16 +68,28 @@ def execute_create_domain(module, asadmin_command, domain_name):
             check_rc=True)
 
 
+def execute_start_domain(asadmin_command, domain_name, module):
+    if module.check_mode is False:
+        module.run_command([asadmin_command, 'start-domain', domain_name], check_rc=True)
+
+
+def execute_stop_domain(asadmin_command, domain_name, module):
+    if module.check_mode is False:
+        module.run_command([asadmin_command, 'stop-domain', domain_name], check_rc=True)
+
+
 def is_existing_domain(module, asadmin_command, domain_name):
     rc, out, err = module.run_command([asadmin_command, 'list-domains'], check_rc=True)
     output_lines = out.split("\n")
     domain_exists = False
+    domain_running = False
     for line in output_lines:
-        match = re.search('^' + domain_name + ' .*', line)
+        match = re.search('^' + domain_name + ' (.*)', line)
         if match is not None:
             domain_exists = True
+            domain_running = match.group(1) == 'running'
             break
-    return domain_exists
+    return domain_exists, domain_running
 
 
 from ansible.module_utils.basic import *
